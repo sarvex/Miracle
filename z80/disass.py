cbe025f18a26ae1fad755526e7069cd018042a37
Sourcery AI <>
2023-05-12 06:40:43 +0000

    'Refactored by Sourcery'

--- z80/disass.py
+++ z80/disass.py
@@ -7,41 +7,47 @@ def main(file):
     line = re.sub('#.*', '', line.strip())
     if not line: continue
     parsed = line.split(' ', 2)
-    if not ' ' in line:
-      print('case %s:' % line)
+    if ' ' not in line:
+      print(f'case {line}:')
       continue
-    number, opcode = parsed[0:2]
+    number, opcode = parsed[:2]
     args = ""
     if len(parsed) >= 3: args = parsed[2]
     if opcode == 'shift':
-      print('case %s: return disassemble_%s(address);' % (number, args))
+      print(f'case {number}: return disassemble_{args}(address);')
       continue
-    print('case %s: res="<span class=opcode>%s</span>";' % (number, opcode))
+    print(f'case {number}: res="<span class=opcode>{opcode}</span>";')
     args = re.sub(r'(REGISTER[HL]?)', r'<span class=register>" + \1 + "</span>', args);
     args = re.sub(r'(AF|BC|DE|HL|SP|PC|IX|IY|(\b[AFBCDEHL]\b))', r'<span class=register>\1</span>', args)
     if 'nnnn' in args:
-      pre = args[0:args.find('nnnn')]
+      pre = args[:args.find('nnnn')]
       post = args[args.find('nnnn') + 4:]
-      print('res += " %s" + addressHtml((readbyte(address + 1) << 8) | readbyte(address)) + "%s"; address += 2;' % (pre, post))
+      print(
+          f'res += " {pre}" + addressHtml((readbyte(address + 1) << 8) | readbyte(address)) + "{post}"; address += 2;'
+      )
     elif 'nn' in args:
-      pre = args[0:args.find('nn')]
+      pre = args[:args.find('nn')]
       post = args[args.find('nn') + 2:]
-      print('res += " %s0x" + hexbyte(readbyte(address)) + "%s"; address += 1;' % (pre, post))
+      print(
+          f'res += " {pre}0x" + hexbyte(readbyte(address)) + "{post}"; address += 1;'
+      )
     elif 'offset' in args:
-      pre = args[0:args.find('offset')]
+      pre = args[:args.find('offset')]
       post = args[args.find('offset') + 6:]
       print('var reladdr = address + 1 + sign_extend(readbyte(address));')
-      print('res += " %s" + addressHtml(reladdr) + "%s"; address += 1;' % (pre, post))
+      print(f'res += " {pre}" + addressHtml(reladdr) + "{post}"; address += 1;')
     elif 'dd' in args:
-      pre = args[0:args.find('+dd')]
+      pre = args[:args.find('+dd')]
       post = args[args.find('+dd') + 3:]
       print('var offset = sign_extend(readbyte(address));')
       print('var sign = offset > 0 ? "+" : "-";')
-      print('res += " %s" + sign + "0x" + hexbyte(offset) + "%s"; address += 1;' % (pre, post))
+      print(
+          f'res += " {pre}" + sign + "0x" + hexbyte(offset) + "{post}"; address += 1;'
+      )
     elif opcode == 'RST':
-      print('res += " " + addressHtml(0x' + args + ');')
+      print(f'res += " " + addressHtml(0x{args});')
     elif args:
-      print('res += " %s";' % args)
+      print(f'res += " {args}";')
     print("break;")
 
 if __name__ == '__main__':
